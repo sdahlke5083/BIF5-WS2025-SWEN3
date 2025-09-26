25e2a2f3d192c7dc3d744e20cc3cd5c8a906aca7
if23b100 <if23b100@technikum-wien.at>
2025-09-26 13:18:05 +0200

    Errors der Controller behoben

---
+++ Paperless/Paperless-REST/API/Controllers/BaseApiController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace API.Controllers
+{
+    public abstract class BaseApiController : ControllerBase
+    {
+        protected IActionResult NotImplementedStub()
+        {
+            return StatusCode(StatusCodes.Status501NotImplemented, new ProblemDetails
+            {
+                Title = "Not implemented",
+                Status = StatusCodes.Status501NotImplemented,
+                Detail = "Stub endpoint —> functionality will be implemented later.",
+                Instance = HttpContext?.Request.Path.Value
+            });
+        }
+    }
+}
+
--- Paperless/Paperless-REST/Program.cs
+++ Paperless/Paperless-REST/Program.cs
@@ -61,7 +61,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
 app.MapControllers();
 app.MapSwagger();
